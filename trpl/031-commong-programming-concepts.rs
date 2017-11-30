4d27917c02eef0c126df2308cb7aa3f59a215da2
nachoba <0800nacho@gmail.com>
2017-11-30 00:35:00 -0300

    more

---
+++ trpl/031-commong-programming-concepts.rs
@@ -0,0 +1,89 @@
+/*
+ *  THE RUST PROGRAMMING LANGUAGE
+ *  CHAPTER 3   ::  COMMON PROGRAMMING CONCEPTS
+ *  ----------------------------------------------------------------------------
+ *  
+ *  Introduction
+ *  ------------
+ *  This chapter introduces concepts that appear in almost every programming
+ *  language but in the context of Rust. We will learn about:
+ *
+ *      * Variables
+ *      * Basic Types
+ *      * Functions
+ *      * Comments
+ *      * Control Flow
+ *
+ *  Keywords
+ *  --------
+ *  Rust has a set of keywords that have been reserved for use by the language
+ *  only. These names cannot be used as names of variables or functions.
+ *  List of reserverd words:
+ *
+ *  as          Primitive casting, disambiguating the specific trait containing
+ *              an item, or renaming items in "use" and "extern crate" state-
+ *              ments.
+ *  break       Exit a loop immediately.
+ *  const       Constant items and constant raw pointers.
+ *  continue    Continue to the next loop iteration.
+ *  crate       External crate linkage or a macro variable representing the
+ *              crate in which the macro is defined.
+ *  else        Fallback for "if" and "if let" control flow statements.
+ *  enum        Defining an enumeration.
+ *  extern      External crate, function, and variable linkage.
+ *  false       Boolean false literal.
+ *  fn          Function definition and function pointer type.
+ *  for         Iterator loop, part of trait impl syntax, and higher-ranked
+ *              lifetime syntax.
+ *  if          Conditional branching.
+ *  impl        Inherent and trait implementation block.
+ *  in          Part of "for" loop syntax.
+ *  let         Variable binding.
+ *  loop        Unconditional, infinite loop.
+ *  match       Pattern matching.
+ *  mod         Module declaration.
+ *  move        Makes a closure take ownership of all its captures.
+ *  mut         Denotes mutability in references, raw pointers, and pattern
+ *              bindings.
+ *  pub         Denotes public visibility in struct fields, "impl" blocks, and
+ *              modules.
+ *  ref         By-reference binding.
+ *  return      Return from function.
+ *  Self        Type alias for the type implementing a trait.
+ *  self        Method subject or current module.
+ *  static      Global variable or lifetime lasting the entire program execution.
+ *  struct      Structure definition.
+ *  super       Parent module of the current module.
+ *  trait       Trait definition.
+ *  true        Boolean true literal.
+ *  type        Type alias and associated type definitions.
+ *  unsafe      Denotes unsafe code, functions, traits, and implementations.
+ *  use         Import symbols into scope.
+ *  where       Type constraint clauses.
+ *  while       Conditional loop.
+ *
+ *  In addition there're some reserverd keywords for future use:
+ *  abstract
+ *  alignof
+ *  become
+ *  box
+ *  do
+ *  final
+ *  macro
+ *  offsetof
+ *  override
+ *  priv
+ *  proc
+ *  pure
+ *  sizeof
+ *  typeof
+ *  unsized
+ *  virtual
+ *  yield
+ *
+ */
+
+ fn main() {
+     println!("Please read comments in the source code:\n");
+ }
+
